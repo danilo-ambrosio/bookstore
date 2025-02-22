de603cc7345c20d1a9bef0893f0ad55ba0441ec4
ambrosde@amazon.com <ambrosde@amazon.com>
2025-02-22 00:17:47 -0300

    Database clean up after each test

--- src/test/java/com/sporty/bookstore/infrastructure/repository/MongoDBExtension.java
+++ src/test/java/com/sporty/bookstore/infrastructure/repository/MongoDBExtension.java
@@ -1,15 +1,53 @@
 package com.sporty.bookstore.infrastructure.repository;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.testcontainers.containers.MongoDBContainer;
 
-public class MongoDBExtension implements BeforeAllCallback {
+import java.io.IOException;
+import java.util.Properties;
+
+public class MongoDBExtension implements BeforeAllCallback, AfterEachCallback {
+
+    private static final String IMAGE_PROPERTY = "spring.data.mongodb.image";
+    private static final String DATABASE_NAME_PROPERTY = "spring.data.mongodb.database";
+    private static final String URI_PROPERTY = "spring.data.mongodb.uri";
+
+    private final MongoDatabase testDatabase;
+    private final MongoDBContainer container;
+
+    public MongoDBExtension() throws IOException {
+        final Properties properties = loadProperties();
+        final String dockerImage = properties.getProperty(IMAGE_PROPERTY);
+        final String databaseName = properties.getProperty(DATABASE_NAME_PROPERTY);
+
+        this.container = new MongoDBContainer(dockerImage);
+        this.container.start();
+
+        try (MongoClient client = MongoClients.create(container.getConnectionString())) {
+            this.testDatabase = client.getDatabase(databaseName);
+        }
+    }
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
-        mongoDBContainer.start();
-        System.setProperty("spring.data.mongodb.uri", mongoDBContainer.getReplicaSetUrl());
+        System.setProperty(URI_PROPERTY, container.getReplicaSetUrl(testDatabase.getName()));
     }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        testDatabase.listCollectionNames().map(testDatabase::getCollection).forEach(MongoCollection::drop);
+    }
+
+    private Properties loadProperties() throws IOException {
+        return PropertiesLoaderUtils.loadProperties(new ClassPathResource("/application.properties"));
+    }
+
 }
