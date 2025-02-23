2879936374ae0e492e96c556fcd3b4dfc5a3bf79
ambrosde@amazon.com <ambrosde@amazon.com>
2025-02-23 08:09:38 -0300

    JavaDoc

--- src/main/java/com/sporty/bookstore/domain/model/pricing/Book.java
+++ src/main/java/com/sporty/bookstore/domain/model/pricing/Book.java
@@ -4,8 +4,8 @@ import com.sporty.bookstore.domain.model.pricing.policy.DiscountPolicy;
 
 /**
  * This class represents a book in the bookstore from a financial perspective,
- * meaning it processes pricing and discounting rules. It contains information
- * about the book's ID, retail price, and discount policy.
+ * meaning it's designed to process pricing and discounting rules. It contains
+ * information about the book's ID, retail price, and discount policy.
  *
  * @author Danilo Ambrosio
  */
@@ -29,6 +29,14 @@ public class Book {
         return new Book(bookId, retailPrice, policy);
     }
 
+    public void reprice(final Price retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+
+    public void apply(final DiscountPolicy policy) {
+        this.discountPolicy = policy;
+    }
+
     public Price deductDiscount(final int quantity, final boolean useLoyaltyPoints) {
         return discountPolicy.deductDiscount(quantity, retailPrice, useLoyaltyPoints);
     }
@@ -42,7 +50,7 @@ public class Book {
     }
 
     public String discountPolicyName() {
-        return discountPolicy.getClass().getSimpleName();
+        return discountPolicy.name().toString();
     }
 
 }
