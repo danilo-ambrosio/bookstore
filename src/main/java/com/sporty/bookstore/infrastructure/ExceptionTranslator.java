eeb5d22197952add917e16359ee76c8414e66f8f
ambrosde@amazon.com <ambrosde@amazon.com>
2025-02-24 14:10:44 -0300

    Exception messages

--- src/main/java/com/sporty/bookstore/infrastructure/ExceptionTranslator.java
+++ src/main/java/com/sporty/bookstore/infrastructure/ExceptionTranslator.java
@@ -2,6 +2,7 @@ package com.sporty.bookstore.infrastructure;
 
 import com.sporty.bookstore.domain.model.inventory.UnavailableBookException;
 import com.sporty.bookstore.domain.model.loyalty.InsufficientLoyaltyPointsException;
+import com.sporty.bookstore.domain.model.pricing.policy.PolicyNotCoveredByLoyaltyProgramException;
 import com.sporty.bookstore.usecase.identity.AuthenticationException;
 import com.sporty.bookstore.usecase.identity.UniqueUsernameException;
 import com.sporty.bookstore.usecase.inventory.BookNotFoundException;
@@ -25,22 +26,27 @@ public class ExceptionTranslator {
 
   @ExceptionHandler(value = BookNotFoundException.class )
   public ResponseEntity<Object> resolveBookNotFoundException() {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book not found");
   }
 
   @ExceptionHandler(value = UnavailableBookException.class )
   public ResponseEntity<Object> resolveUnavailableBookException() {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book is not in stock");
   }
 
   @ExceptionHandler(value = UniqueUsernameException.class )
   public ResponseEntity<Object> resolveUniqueUsernameException() {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
   }
 
   @ExceptionHandler(value = InsufficientLoyaltyPointsException.class )
   public ResponseEntity<Object> resolveInsufficientLoyaltyPointsException() {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Beneficiary does not have enough points to purchase the book");
+  }
+
+  @ExceptionHandler(value = PolicyNotCoveredByLoyaltyProgramException.class )
+  public ResponseEntity<Object> resolvePolicyNotCoveredByLoyaltyProgramException() {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Given book is not covered by loyalty program");
   }
 
 }
